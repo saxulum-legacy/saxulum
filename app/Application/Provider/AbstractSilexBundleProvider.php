059a09c669800e1d465817492dc37ab4bf99ff2f
Dominik Zogg <dominik.zogg@gmail.com>
2013-06-20 21:01:25 +0200

    enhance code and add caching

--- app/Application/Provider/AbstractSilexBundleProvider.php
+++ app/Application/Provider/AbstractSilexBundleProvider.php
@@ -32,10 +32,9 @@ abstract class AbstractSilexBundleProvider implements ServiceProviderInterface
 
     protected function addController()
     {
-        $app = $this->app;
-        $controllerMapJson = $app['cache_dir'] . '/controllerMap.json';
-        if(file_exists($controllerMapJson)) {
-            $controllerMap = json_decode(file_get_contents($controllerMapJson));
+        $controllerMapJson = $this->getCacheDir() . '/controller.map.json';
+        if (!$this->app['debug'] && is_file($controllerMapJson)) {
+            $controllerMap = json_decode(file_get_contents($controllerMapJson), true);
         } else {
             $controllerMap = array();
             foreach (glob($this->getPath() . '/Controller/*Controller.php') as $controllerFilePath) {
@@ -51,37 +50,60 @@ abstract class AbstractSilexBundleProvider implements ServiceProviderInterface
             file_put_contents($controllerMapJson, json_encode($controllerMap));
         }
 
-        foreach($controllerMap as $serviceId => $controllerNamespace) {
-            $app[$serviceId] = $app->share(function() use ($app, $controllerNamespace) {
+        $app = $this->app;
+        foreach ($controllerMap as $serviceId => $controllerNamespace) {
+            $this->app[$serviceId] = $this->app->share(function() use ($app, $controllerNamespace) {
                 return new $controllerNamespace($app);
             });
-            $controllerNamespace::addRoutes($app, $serviceId);
+            $controllerNamespace::addRoutes($this->app, $serviceId);
         }
     }
 
     protected function addDoctrineOrmMappings()
     {
-        $emsOptions = isset($this->app['orm.em.options']) ? $this->app['orm.em.options'] : array();
-        $emsOptions['mappings'][] = array(
-            'type' => 'annotation',
-            'namespace' => $this->getNamespace() . '\Entity',
-            'path' => $this->getPath() .'/Entity',
-            'use_simple_annotation_reader' => false,
-        );
-        $this->app['orm.em.options']= $emsOptions;
+        if (!isset($this->app['orm.em.options'])) {
+            $this->app['orm.em.options'] = array(
+                'mappings' => array(),
+            );
+        }
+
+        $this->app['orm.em.options'] = array_merge_recursive($this->app['orm.em.options'], array(
+            'mappings' => array(
+                array(
+                    'type' => 'annotation',
+                    'namespace' => $this->getNamespace() . '\Entity',
+                    'path' => $this->getPath() .'/Entity',
+                    'use_simple_annotation_reader' => false,
+                )
+            ),
+        ));
     }
 
     protected function addTranslatorRessources()
     {
-        $path = $this->getPath();
+        $translationMapJson = $this->getCacheDir() . '/translation.map.json';
+        if (!$this->app['debug'] && is_file($translationMapJson)) {
+            $translationMap = json_decode(file_get_contents($translationMapJson), true);
+        } else {
+            $translationMap = array();
+            foreach (glob($this->getPath() . '/Resources/translations/*.yml') as $yamlFilePath) {
+                $domainAndLocale = explode('.', basename($yamlFilePath, '.yml'));
+                if (count($domainAndLocale) == 2) {
+                    $translationMap[] = array(
+                        'path' => $yamlFilePath,
+                        'locale' => $domainAndLocale[1],
+                        'domain' => $domainAndLocale[0]
+                    );
+                }
+            }
+            file_put_contents($translationMapJson, json_encode($translationMap));
+        }
+
         $this->app['translator'] = $this->app->share($this->app->extend('translator',
-            function(Translator $translator) use ($path) {
+            function(Translator $translator) use ($translationMap) {
                 $translator->addLoader('yaml', new YamlFileLoader());
-                foreach (glob($path . '/Resources/translations/*.yml') as $yamlFilePath) {
-                    $domainAndLocale = explode('.', basename($yamlFilePath, '.yml'));
-                    if (count($domainAndLocale) == 2) {
-                        $translator->addResource('yaml', $yamlFilePath, $domainAndLocale[1], $domainAndLocale[0]);
-                    }
+                foreach ($translationMap as $translation) {
+                    $translator->addResource('yaml', $translation['path'], $translation['locale'], $translation['domain']);
                 }
 
                 return $translator;
@@ -133,6 +155,19 @@ abstract class AbstractSilexBundleProvider implements ServiceProviderInterface
     protected function namespaceToServiceId($namespace)
     {
         return strtolower(str_replace("\\", '.', $namespace));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getCacheDir()
+    {
+        $cacheDir = $this->app['cache_dir'] . '/' . $this->namespaceToServiceId($this->getNamespace());
+        if (!is_dir($cacheDir)) {
+            mkdir($cacheDir, 0777, true);
+        }
+
+        return $cacheDir;
     }
 
     protected function assignReflectionClass()
