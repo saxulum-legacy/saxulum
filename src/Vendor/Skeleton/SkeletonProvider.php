53d02400e03fcd9aed7b9ca36435e6a2c52aef8a
Dominik Zogg <dominik.zogg@gmail.com>
2013-06-30 01:45:00 +0200

    no need for a controller as a service test

--- src/Vendor/Skeleton/SkeletonProvider.php
+++ src/Vendor/Skeleton/SkeletonProvider.php
@@ -2,33 +2,7 @@
 
 namespace Vendor\Skeleton;
 
-use Saxulum\SaxulumFramework\ControllerMap\ControllerMap;
-use Saxulum\SaxulumFramework\ControllerMap\Controller;
-use Saxulum\SaxulumFramework\ControllerMap\Method;
 use Saxulum\SaxulumFramework\Provider\AbstractSilexBundleProvider;
+use Silex\Application;
 
-class SkeletonProvider extends AbstractSilexBundleProvider
-{
-    /**
-     * @return ControllerMap
-     */
-    protected function getControllerMap()
-    {
-        $controllerMap = parent::getControllerMap();
-
-        $controller = new Controller();
-        $controller->setServiceId("vendor.skeleton.controllerservice.testcontroller");
-        $controller->setNamespace("Vendor\\Skeleton\\ControllerService\\TestController");
-        $controller->setInjectionKeys(array('doctrine'));
-
-        $method = new Method();
-        $method->setName('setTwig');
-        $method->setInjectionKeys(array('twig'));
-
-        $controller->addMethod($method);
-
-        $controllerMap->addController($controller);
-
-        return $controllerMap;
-    }
-}
+class SkeletonProvider extends AbstractSilexBundleProvider {}
