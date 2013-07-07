e08926fe8ffdce57a6d8fa310ce50b48b3cd6b2e
Dominik Zogg <dominik.zogg@gmail.com>
2013-07-07 20:47:02 +0200

    remove not used use statement

--- src/Vendor/Skeleton/SkeletonProvider.php
+++ src/Vendor/Skeleton/SkeletonProvider.php
@@ -3,6 +3,5 @@
 namespace Vendor\Skeleton;
 
 use Saxulum\SaxulumFramework\Provider\AbstractSilexBundleProvider;
-use Silex\Application;
 
 class SkeletonProvider extends AbstractSilexBundleProvider {}
